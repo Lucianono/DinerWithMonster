fb2bbce430bdf92151a2d409bcaccf5b20b12f82
Bryyyyy <bryancalulo@gmail.com>
2022-07-11 21:56:18 +0800

    player controlled move and slide

--- main-sprite.gd
+++ main-sprite.gd
@@ -1,18 +1,22 @@
 extends KinematicBody2D
 
 onready var raycast_right = get_node("RayCast2D") 
+
 var isPlayerColliding = false
-var kboard
+var kboard = Vector2(0,0)
 export var speed = 200
 
 func _ready():
-	set_process(true)
+	set_physics_process(true)
 	
-func _process(delta):
-	kboard = Vector2(0,0)
+func _physics_process(delta):
+	
 	kboard.x = int(Input.is_action_pressed("ui_right"))-int(Input.is_action_pressed("ui_left"))
 	kboard.y = int(Input.is_action_pressed("ui_down"))-int(Input.is_action_pressed("ui_up"))
-	move(position + (kboard * speed * delta))
+	kboard = kboard.normalized() * speed
+	kboard = move_and_slide(kboard)
+	
+	print("h")
 	
 	
 	
