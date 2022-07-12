8774c0de414e38a0953f726feacc97f88001e23f
Bryyyyy <bryancalulo@gmail.com>
2022-07-12 20:30:05 +0800

    signal and connect added (not working)
    >:((((

--- egg-coop.gd
+++ egg-coop.gd
@@ -1,5 +1,6 @@
 extends StaticBody2D
 
+signal coop_entered
 
 func _ready():
 	pass
@@ -7,4 +8,6 @@ func _ready():
 
 func _on_Area2D_area_entered(area):
 	if(area.get_name() == "caldo-area"):
+		emit_signal("coop_entered")
+		
 		print(area.get_name())
