aca264d322ccb27e68ed89bc4e4d14af2e9404d3
Bryyyyy <bryancalulo@gmail.com>
2022-07-12 19:25:44 +0800

    caldo will not go outside view

--- main-sprite.gd
+++ main-sprite.gd
@@ -1,20 +1,27 @@
 extends KinematicBody2D
 
-onready var raycast_right = get_node("RayCast2D") 
-
-var isPlayerColliding = false
 var kboard = Vector2(0,0)
+var screensize
+var sprite_extent
 export var speed = 200
 
 func _ready():
+	sprite_extent = get_node("caldo-area/Sprite").get_texture().get_size()/2
+	screensize = get_viewport_rect().size
 	set_physics_process(true)
 	
-func _physics_process(delta):
+	
+func _physics_process(_delta):
 	
 	kboard.x = int(Input.is_action_pressed("ui_right"))-int(Input.is_action_pressed("ui_left"))
 	kboard.y = int(Input.is_action_pressed("ui_down"))-int(Input.is_action_pressed("ui_up"))
 	kboard = kboard.normalized() * speed
 	kboard = move_and_slide(kboard)
+	
+	position.y = clamp(position.y, 1 + sprite_extent.y, screensize.y - sprite_extent.y)
+	position.x = clamp(position.x, 1 + sprite_extent.x, screensize.x - sprite_extent.x)
+	
+	
 	
 	
 	
