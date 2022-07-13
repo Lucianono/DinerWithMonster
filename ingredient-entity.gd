e6346dca66e1e295f0ad3c260ae2e5f76bd1a47f
Bryyyyy <bryancalulo@gmail.com>
2022-07-13 21:25:00 +0800

    attempt to sync player and ingr

--- ingredient-entity.gd
+++ ingredient-entity.gd
@@ -1,11 +1,18 @@
 extends Area2D
 
+onready var mainPlayer = preload("res://main-sprite.tscn")
+var isPlayerHolding = false
 
 func _ready():
+	print(get_tree().get_root().get_node("Caldo-player").get_position())
 	pass
 
 
 func _on_ingredientdrop_area_entered(area):
 	if area.get_name() == "caldo-area" :
+		isPlayerHolding = true
 		print("wow")
-		queue_free()
+
+func _process(_delta):
+	if isPlayerHolding:
+		set_position(get_position() + mainPlayer.instance().get_position() + Vector2(2,2))
