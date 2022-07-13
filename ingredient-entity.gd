f4bf34611ba5283043c32352f976fcd4b82afdee
Bryyyyy <bryancalulo@gmail.com>
2022-07-13 20:50:04 +0800

    spawn ingredient and detect player

---
+++ ingredient-entity.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+
+func _ready():
+	pass
+
+
+func _on_ingredientdrop_area_entered(area):
+	if area.get_name() == "caldo-area" :
+		queue_free()
--- on-site.gd
+++ on-site.gd
@@ -1,12 +1,20 @@
 extends Node
 
 
-onready var ingr_ent = preload("res://ingredient-entity.tscn")
+onready var ingr = preload("res://ingredient-entity.tscn")
+onready var coop = preload("res://egg-coop.tscn")
+var coop_position
 
 func _ready():
+	coop_position = get_node("Coop").get_position()
+	print(coop_position)
+	randomize()
 	set_process(true)
 	
 
 func _on_Coop_coop_entered():
+	var ingr_ent = ingr.instance()
+	add_child(ingr_ent)
+	ingr_ent.set_position(coop_position + Vector2(20,50))
 	print("caldo_entered")
 	
