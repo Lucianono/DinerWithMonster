eb001d1624f3f71c5cdd5074268ff04f35ba99b5
Bryan Calulo <bryancalulo@gmail.com>
2022-07-14 10:27:05 +0800

    initializing ingr name

--- ingredient-entity.gd
+++ ingredient-entity.gd
@@ -5,12 +5,14 @@ var isPlayerHolding = false
 func _ready():
 	print(get_node("/root/Node2D/Caldo-player").get_position())
 	pass
+	
+func initIngrName(ingr_name):
+	print(ingr_name)
 
 
 func _on_ingredientdrop_area_entered(area):
 	if area.get_name() == "caldo-area" :
 		isPlayerHolding = true
-		print("wow")
 
 func _process(_delta):
 	if isPlayerHolding:
