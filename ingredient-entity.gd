1a4d8513aa812c65d026ca97a06be942a0ab4725
Bryyyyy2 <bryancalulo@gmail.com>
2022-07-14 16:37:47 +0800

    added dumb aiming

--- ingredient-entity.gd
+++ ingredient-entity.gd
@@ -5,6 +5,8 @@ var isPlayerThrowing = false
 var global_ingr_name = null
 var screensize
 
+var slope_vector
+
 func _ready():
 	screensize = get_viewport_rect().size
 	print(get_node("/root/Node2D/Caldo-player").get_position())
@@ -23,13 +25,13 @@ func _on_ingredientdrop_area_entered(area):
 	if area.get_name() == "caldo-area" && !isPlayerThrowing:
 		isPlayerHolding = true
 
-func _process(_delta):
+func _process(delta):
 	if isPlayerHolding && !isPlayerThrowing:
 		set_position(get_node("/root/Node2D/Caldo-player").get_position()+Vector2(70,0))
 	elif !isPlayerHolding && isPlayerThrowing:
-		set_position(position + Vector2(10,0))
+		set_position(position + slope_vector * delta)
 		
-	if position.x > screensize.x:
+	if position.x > screensize.x || position.y > screensize.y:
 		queue_free()
 			
 func _unhandled_input(event):
@@ -39,4 +41,10 @@ func _unhandled_input(event):
 				print("ingr freed ", event.position)
 				isPlayerHolding = false
 				isPlayerThrowing = true
+				
+				var slope = (event.position.y - position.y)/(event.position.x - position.x)
+				var yintercept = position.y - (slope * position.x)
+				slope_vector = Vector2(200,(slope * event.position.x)+yintercept)
+				print(slope,  " and " ,yintercept)
+			
 		
