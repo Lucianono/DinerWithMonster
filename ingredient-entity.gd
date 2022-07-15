e13503ff2a8e3f30de1d9269d85b2269ed503e7f
Bryyyyy2 <bryancalulo@gmail.com>
2022-07-15 10:13:06 +0800

    upgraded aiming

--- ingredient-entity.gd
+++ ingredient-entity.gd
@@ -4,10 +4,9 @@ var isPlayerHolding = false
 var isPlayerThrowing = false
 var global_ingr_name = null
 var screensize
+var left_click_event
 
-var slope_vector
 var slope
-var yintercept
 
 func _ready():
 	screensize = get_viewport_rect().size
@@ -31,7 +30,11 @@ func _process(delta):
 	if isPlayerHolding && !isPlayerThrowing:
 		set_position(get_node("/root/Node2D/Caldo-player").get_position()+Vector2(70,0))
 	elif !isPlayerHolding && isPlayerThrowing:
-		set_position(position + Vector2(200,(slope * position.x)+yintercept) * delta)
+		if left_click_event.position.x > position.x:
+			set_position(position + Vector2(1,-slope)* 500 * delta)
+		else:
+			set_position(position + Vector2(-1,-slope)* 500 * delta)
+		
 		
 	if position.x > screensize.x || position.y > screensize.y:
 		queue_free()
@@ -43,9 +46,9 @@ func _unhandled_input(event):
 				print("ingr freed ", event.position, position)
 				isPlayerHolding = false
 				isPlayerThrowing = true
+				left_click_event = event
 				
 				slope = (position.y - event.position.y)/(event.position.x - position.x)
-				yintercept = -position.y - (slope * position.x)
-				print(slope,  " and " ,yintercept)
+				print(slope)
 			
 		
