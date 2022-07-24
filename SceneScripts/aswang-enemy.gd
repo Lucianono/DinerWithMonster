da81e3d1facfdd414504510d9b4f716ee9b48897
Bryyyyy <bryancalulo@gmail.com>
2022-07-24 14:48:15 +0800

    small tweaks

--- SceneScripts/aswang-enemy.gd
+++ SceneScripts/aswang-enemy.gd
@@ -1,38 +1,25 @@
 extends KinematicBody2D
 
-
+var speed = 1000
 var slope
-var slope_vector =0.1
+var slope_vector
 
 var isClickOnRight = true
 
 func _ready():
+	set_physics_process(true)
 	pass
 	
-func _process(delta):
+func _physics_process(delta):
 	isClickOnRight = get_node("/root/Node2D/Caldo-player").get_position().x > position.x
 	
 	slope = (position.y - get_node("/root/Node2D/Caldo-player").get_position().y)/(get_node("/root/Node2D/Caldo-player").get_position().x - position.x)
 	slope_vector = Vector2(1, slope).normalized()
-	print(slope_vector)
 	
 	
 	#for smart holding and throwing
 	if isClickOnRight:
-		move_and_slide(Vector2(1,-1) * slope_vector * 10000 * delta)
+		move_and_slide(Vector2(1,-1) * slope_vector * speed * delta)
 	else:
-		move_and_slide(Vector2(-1,1) * slope_vector * 10000 * delta)
+		move_and_slide(Vector2(-1,1) * slope_vector * speed * delta)
 		
-
-#for left clicking
-func _unhandled_input(event):
-	if event is InputEventMouseButton:
-		if event.button_index == BUTTON_LEFT :
-			if event.pressed:
-				
-				isClickOnRight = get_node("/root/Node2D/Caldo-player").get_position().x > position.x
-				
-				#may math shit for aiming shit AHHAHAHHAHA
-				slope = (position.y - get_node("/root/Node2D/Caldo-player").get_position().y)/(get_node("/root/Node2D/Caldo-player").get_position().x - position.x)
-				slope_vector = Vector2(1, slope).normalized()
-				print(slope_vector)
