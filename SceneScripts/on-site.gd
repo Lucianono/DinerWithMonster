7cdca5e3d824a1e4bb462a3211ec5cab7e9b810e
Bryyyyy <bryancalulo@gmail.com>
2022-07-27 13:27:16 +0800

    working reassemble line with for loops

--- SceneScripts/on-site.gd
+++ SceneScripts/on-site.gd
@@ -164,18 +164,16 @@ func customer_assign():
 
 	
 func reassemble_line(col,row):
-	
-	
-		if col < 2 && arr_cust_line[col+1][row] != null :
+	for i in cust_column - 1:
+		if col < cust_column - (i + 1) && arr_cust_line[col+i+1][row] != null :
 				
-			arr_cust_line[col][row] = arr_cust_line[col+1][row]
-			arr_cust_line[col+1][row] = null
-			arr_cust_line[col][row].initCustIndex(col,row)
+			arr_cust_line[col+i][row] = arr_cust_line[col+i+1][row]
+			arr_cust_line[col+i+1][row] = null
+			arr_cust_line[col+i][row].initCustIndex(col+i,row)
+	
+	cust_col_ctr = col
 				
-			if col < 1 && arr_cust_line[col+2][row] != null :
-					arr_cust_line[col+1][row] = arr_cust_line[col+2][row]
-					arr_cust_line[col+2][row] = null
-					arr_cust_line[col+1][row].initCustIndex(col+1,row)
+				
 			
 
 
